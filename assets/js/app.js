b30f1ae75edfd6c7342db2b966df93b1dcafdea8
Kevin Walton <kevywalton@gmail.com>
2019-03-25 17:17:16 +0000

    Created js file & added some click events to test button divs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
-$('#topLeft').click(function() {
+$('#red').click(function() {
     console.log('top left clicked!');
+});
+
+$('#green').click(function() {
+    console.log('top right clicked!');
+});
+
+$('#blue').click(function() {
+    console.log('bottom right clicked!');
+});
+
+$('#yellow').click(function() {
+    console.log('bottom left clicked!');
 });
