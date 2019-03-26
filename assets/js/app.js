af6ebf9cdaa45a0c6e456aef706d856dfe858ab3
Kevin Walton <kevywalton@gmail.com>
2019-03-26 17:07:27 +0000

    Created placeholder variables & started my jQuery click events

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,69 @@
-$('#red').click(function() {
-    console.log('top left clicked!');
+// Variables
+
+let sequence = [];
+let playerSeq = [];
+let flash;
+let count;
+let correctSeq;
+let compTurn;
+let strict = false;
+let sound = true;
+let on = false;
+let win;
+
+// Element Referencing Variables
+
+const turnCount = document.querySelector("#count"); // Score Counter
+const topLeft = document.querySelector("#red"); //redBtn
+const topRight = document.querySelector("#green"); // greenBtn
+const bottomLeft = document.querySelector("#yellow"); // yellowBtn
+const bottomRight = document.querySelector("#blue"); // blueBtn
+const strictBtn = document.querySelector("#strictOn");
+const onBtn = document.querySelector("#powerOn");
+const startBtn = document.querySelector("#startBtn");
+
+// ***** EVENT LISTENERS *****
+
+// Switch Power to Game on
+
+$("#powerOn").click(function() {
+    if (onBtn.checked == true) {
+        on = true;
+        turnCount.textContent = "HI";
+        setTimeout(clearTurnCount, 1500);
+    } else {
+        on = false;
+        turnCount.textContent = "";
+        clearColor();
+    }
 });
 
-$('#green').click(function() {
-    console.log('top right clicked!');
+// Switch Strict Mode on
+
+$("#strictOn").click(function() {
+    if (strictBtn.checked == true) {
+        strict = true;
+    } else {
+        strict = false;
+    }
 });
 
-$('#blue').click(function() {
-    console.log('bottom right clicked!');
+// Start Button to Initialize the Game
+
+$(".startBtn").click(function() {
+    if (on || win) {
+        play();
+    }
 });
 
-$('#yellow').click(function() {
-    console.log('bottom left clicked!');
-});
+// Reset all variables for New Game
+function play() {
+    
+}
+
+
+
+
+
+
+
